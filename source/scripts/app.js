c10ca4841ab57987c15dc0849fbfe1ae406da0b9
Simon Buerger <simon@bluegrassdigital.com>
2017-08-07 10:43:13 +0200

    Removed domReady and some sass lint config

--- source/scripts/app.js
+++ source/scripts/app.js
@@ -13,13 +13,10 @@ registry.add({
 // Parse the body element
 parser.parse()
 
-// Anything that needs dom ready
-dom.onReady(() => {
-  /* All tables in the rte or with the .table class will be wrapped with a div with
-     class table-overflow automatically to allow horizontal scrolling tables on mobile */
-  const tables = document.querySelectorAll('.rte table, .table')
+/* All tables in the rte or with the .table class will be wrapped with a div with
+ class table-overflow automatically to allow horizontal scrolling tables on mobile */
+const tables = document.querySelectorAll('.rte table, .table')
 
-  for (let table of tables) {
-    dom.wrap(table, html => `<div class="table-overflow">${html}</div>`)
-  }
-})
+for (let table of tables) {
+  dom.wrap(table, html => `<div class="table-overflow">${html}</div>`)
+}
